cf8b26adb432379fcca0029915ef5b70b3b82892
NoorUlAinAfaq <59082791+NoorUlAinAfaq@users.noreply.github.com>
2020-06-22 00:36:24 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
       <div className="container">
         <div className="app-wrapper">
           <Header />
-          <Balances />
+          <Balance />
           <AddTransaction />
           <IncomeList /> 
           <ExpenseList />
